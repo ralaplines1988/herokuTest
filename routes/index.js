4e7cde1f5e20727ef17b75a2df697018e86774dc
Nick Chen <55172429+ralaplines1988@users.noreply.github.com>
2021-11-02 22:39:08 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,15 @@ router.get("/", function (req, res) {
   res.render("pages/index");
 });
 
+router.get("/hello", function (req, res) {
+  res.send("Hello Nodejs");
+});
+
+router.post("/sum", function (req, res) {
+  var n1 = parseInt(req.body.num1);
+  var n2 = parseInt(req.body.num2);
+  var sum = n1 + n2;
+  res.json({ mysum: sum });
+});
+
 module.exports = router;
